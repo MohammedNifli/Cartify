1baeda42b74500f6099ff4583b9629fbb24abe8f
Mohammed Nifli <mohammednifliap@gmail.com>
2024-03-13 14:18:42 +0530

    sixth comit

--- index.js
+++ index.js
@@ -39,10 +39,7 @@ app.use((req,res,next)=>{
 
 })
 
-app.use((req,res,next)=>{
-    res.set('Cache-control','no-store,no-cache');
-    next();
-  });
+
 
 
   const disableCache = (req, res, next) => {
